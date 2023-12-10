45bbfa2bdc77ed4d106fc984d64a67c78329ec05
Ruben Rodriguez <rrodriguez6@outlook.com>
2023-12-10 14:40:25 -0600

    publishing downloadTopic

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
+const client = new SNSClient({ region: process.env.AWS_REGION | "us-east-2" });
+const {downloadTopic} = JSON.parse(process.env.COPILOT_SNS_TOPIC_ARNS);
+
+app.use(express.json())
+
+app.get('/', (_req, res) => {
+  res.send('Downtube API is running!')
+})
+
+app.post('/download', async (req, res) => {
+  const videoId = req.body.video
+  await client.send(new PublishCommand({
+    Message: videoId,
+    TopicArn: downloadTopic,
+  }));
+  res.status(201)
 })
 
 app.listen(port, () => {
